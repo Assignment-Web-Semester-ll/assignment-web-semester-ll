e7dd23ba6ba04c12af8525046470cc8ba4e7f7d9
kimchhenglim <goodboy00710@yahoo.com>
2020-02-06 20:34:59 +0700

    Blah

---
+++ resources/views/adminpage/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.adminpagelayouts.master')
+
+@section('title')
+    Home
+@endsection
+
+{{-- @section('content')
+    <div class="row">
+            
+    </div>
+@endsection
+
+@section('scripts')
+
+@endsection --}}
--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,4 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
-Route::get('/frontend', function () {
-    return view('frontend.index');
-});
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/dashboard', function () { return view('admin.dashboard'); });
-Route::get('/user', 'Admin\DashboardController@registered');
-Route::get('/edit-user', 'Admin\DashboardController@editUser');
-Route::post('/update-user', 'Admin\DashboardController@updateUser');
-Route::delete('/delete-user', 'Admin\DashboardController@deleteUser');
-// Route::group(['middleware' => ['admin']], function(){
-//     Route::get('/dashboard', function () { return view('admin.dashboard'); });
-//     Route::get('/user', 'Admin\DashboardController@registered');
-//     Route::get('/edit-user', 'Admin\DashboardController@editUser');
-//     Route::post('/update-user', 'Admin\DashboardController@updateUser');
-//     Route::delete('/delete-user', 'Admin\DashboardController@deleteUser');
-// });
+Route::get('/', function () { return view('adminpage.home'); });
