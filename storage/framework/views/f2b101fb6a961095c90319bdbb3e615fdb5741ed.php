7a65fe14ac5a12b2ac8c79d2b6019666f9cd1cde
kimchhenglim <goodboy00710@yahoo.com>
2020-03-22 21:35:22 +0700

    Clear

--- storage/framework/views/f2b101fb6a961095c90319bdbb3e615fdb5741ed.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('title'); ?>
-    Dashboard
-<?php $__env->stopSection(); ?>
-
-
-<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Year (Prepare for work)\Laravel\Assignment_Sokim\assignment-web-semester-ll\resources\views/admin/dashboard.blade.php ENDPATH**/ ?>
